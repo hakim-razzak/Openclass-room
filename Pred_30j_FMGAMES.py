0cd730d207d0d566e821cc8f162d6a91b2cb1ee8
hakim-razzak <44054918+hakim-razzak@users.noreply.github.com>
2019-03-21 16:32:36 +0100

    Add files via upload

---
+++ Pred_30j_FMGAMES.py
@@ -0,0 +1,164 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  1 10:38:04 2019
+
+@author: Hakim Razzak
+"""
+from dataiku import pandasutils as pdu
+import dataiku
+
+import pandas as pd, numpy as np
+
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+from sklearn import datasets, linear_model
+from sklearn import metrics
+from sklearn import model_selection
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+
+
+dataset_df = pd.read_csv('C:/Users/Hakim Razzak/Documents/Analyse_Retention_FMgames/dataset_data_attr_activity_filtered2_6months.csv', sep=',')
+dataset_df.head()
+
+
+def selection_cohorte(nom_application, pays, suppression_dau, sort_by, a):
+    
+    global df
+    
+    df = dataset_df[dataset_df['dim_country'] == pays]
+    df = df.rename(columns=lambda x: x.replace('_sum', ''))
+    df = df[df['dim_app'].str.contains(nom_application)]
+    
+    if suppression_dau == 1:
+        df = df.drop('avg_dau', 1)
+    
+    df = df.sort_values(by = sort_by, ascending=a)
+    
+    return df
+
+
+selection_cohorte('Shozila', 'US', 1, 'week', False)
+
+
+
+def Prediction_Day30max(nb_jour_pred,first_col_retention,last_col_retention):
+    
+    list_quality_pred = []
+    i = 0
+    j = 0
+    
+    for j in range(nb_jour_pred):
+
+        if i == 0:
+        
+            df_features = df.values[:,first_col_retention:last_col_retention]
+            df_features = pd.DataFrame(df_features)
+            df_target = df.values[:,last_col_retention]
+            df_target = pd.DataFrame(df_target)
+
+            df_prepared = pd.concat([df_target, df_features], axis=1)
+            df_prepared.head()
+            
+        else:
+            
+            df_features = df_predict
+            df_target = df.values[:,last_col_retention+j]
+            df_target = pd.DataFrame(df_target)
+            
+            df_prepared = pd.concat([df_target, df_features], axis=1)
+            df_prepared.head()
+
+
+        data = df_prepared.as_matrix()
+        X = data[:,1:]
+        Y = data[:,0]
+
+        X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X,Y,test_size = 0.3, random_state=0)
+        print(X_train.shape, X_test.shape, Y_train.shape, Y_test.shape)
+
+
+        # Create linear regression object
+        regr = linear_model.LinearRegression()
+
+        # Train the model using the training sets
+        regr.fit(X_train, Y_train)
+
+        # Make predictions using the testing set
+        y_pred = regr.predict(X_test)
+
+
+        # The coefficients
+        print('Coefficients:', regr.coef_)
+        # The mean squared error
+        print("Mean squared error: %.2f"% mean_squared_error(Y_test, y_pred))
+        # Explained variance score: 1 is perfect prediction
+        r2 = r2_score(Y_test, y_pred)
+        print('Variance score: %.2f' % r2)
+
+        print(y_pred)
+
+
+        pred = regr.predict(X)
+
+        X_df = pd.DataFrame(X)
+        pred_df = pd.DataFrame(pred)
+
+        df_predict = pd.concat([X_df, pred_df], axis=1, ignore_index=True)
+
+        df_predict
+        
+        i =+ 1
+        
+        list_quality_pred.append(r2)
+        
+    return df_predict, list_quality_pred
+    
+
+
+df_predict, list_quality_pred = Prediction_Day30max(21,5,14)
+
+df_predict
+list_quality_pred
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Pred_suite_geo_calcul_alpha.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  8 11:11:12 2019
+
+@author: Hakim Razzak
+"""
+
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+import matplotlib.pyplot as plt
+
+# Read recipe inputs
+dataset_data_attr_activity_filtered2_6months = dataiku.Dataset("dataset_data_attr_activity_filtered2_6months")
+dataset_df = dataset_data_attr_activity_filtered2_6months.get_dataframe()
+
+
+df = dataset_df[dataset_df['dim_country'] == 'US']
+df = df[df['dim_app'].str.contains('Alizoha')]
+df = df.rename(columns=lambda x: x.replace('_sum', ''))
+df = df.drop('avg_dau', 1)
+df = df.sort_values(by = 'week', ascending=False)
+df
+
+
+df_train = df.values[0:8,]
+df_train = pd.DataFrame(df_train)
+df_train
+
+#%%
+def weighted_average(data, col_install, col_retention):
+    i=0
+    val_eff=[]
+    m=[]
+
+    for j in data.columns[col_retention:]:
+        val_eff=[]
+
+        for i in range(len(data[col_install])):
+            val_eff.append(float(data[col_install][i])*data[j][i])
+
+        m.append(sum(val_eff)/sum(data[col_install]))
+
+    return(m)
+    
+def calcul_alpha():
+    alpha=[]
+    i=0
+
+    for i in range(len(m)-1):
+        alpha.append(float(m[i+1])/m[i])
+
+    return(alpha)
+    
+def model(col_retention, row_pred):
+    val_col=[]
+    Un=[]
+
+    for j in df_train.columns[col_retention:]:
+        val_col.append(float(df_train[j][row_pred]))
+    for i in range(len(alpha)):
+        Un.append(float(val_col[i])*alpha[i])
+
+    return Un
+
+def graph(col_retention,row_pred,Un):
+    val_col=[]
+    for j in df_train.columns[col_retention:]:
+        val_col.append(float(df_train[j][row_pred]))
+
+    plt.plot(df_train.columns[col_retention+1:],val_col[:-1])
+    plt.plot(df_train.columns[col_retention+1:],Un)
+
+    plt.show()
+
+    return val_col
+
+#%%
+    
+m = weighted_average(df_train, 4, 5)
+
+alpha = calcul_alpha()
+print (alpha)
+
+Un = model(5,0)
+print(Un)
+
+val = graph(5,0,Un)
+
+df = pd.DataFrame({'Jour-J':val[:-1], 'Alpha':alpha, 'Predj+1':Un})
+df
+
+
+
+
